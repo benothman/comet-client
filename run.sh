3b95f9f5391715697ea4b87c202acb7b4f895881
benothman <nabil.benothman@gmail.com>
2012-10-30 17:01:31 +0100

    Update client app & scripts

--- run.sh
+++ run.sh
@@ -10,19 +10,19 @@ n=$2;
 #max=$3;
 delay=$3;
 nb_req=$4;
-
+nb_clients=$5;
 
 
 #filename=$n-$max-$delay-$(date +%s)-log.txt
-filename=$n-$delay-$(date +%s)-log.txt
+filename=$n-$delay-$nb_req-$nb_clients-$(date +%s)-log.txt
 echo $filename;
 
 log_file=$(date +%s)-log.txt
 
 #mvn exec:java -Dexec.mainClass="org.jboss.web.comet.CometServletClientTest" -Dexec.args="$url $n $max $delay" > $log_file
-mvn exec:java -Dexec.mainClass="org.jboss.web.comet.CometServletClientTest" -Dexec.args="$url $n $delay" > $log_file
+mvn exec:java -Dexec.mainClass="org.jboss.web.comet.CometServletClientTest" -Dexec.args="$url $n $delay $nb_req $nb_clients" > $log_file
 
-printf "max \t min \t avg\n" > $filename
+#printf "max \t min \t avg\n" > $filename
 egrep -v '[a-zA-Z]|^\s*$' $log_file >> $filename
 
-mvn exec:java -Dexec.mainClass="org.jboss.web.comet.LogParser" -Dexec.args="$filename $nb_req"
+#mvn exec:java -Dexec.mainClass="org.jboss.web.comet.LogParser" -Dexec.args="$filename $nb_req"
